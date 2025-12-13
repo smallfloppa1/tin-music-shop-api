a63ef8d059aecd74765e063b21f5e98520d7f1cd
Denys Dovgan <ytcsgoatlas@gmail.com>
2025-12-13 18:04:20 +0100

    delete compiled files

--- backend/dist/modules/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from './entity/user.entity';
-import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
-export declare class UserService {
-    private userRepository;
-    constructor(userRepository: Repository<User>);
-    create(createUserDto: CreateUserDto): Promise<User>;
-}
